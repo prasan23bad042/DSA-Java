f4d84644173c461aba1b0568ec01f3cc0e6a6fb8
sai prasan <kit27.ad42@gmail.com>
2026-01-12 22:32:19 +0530

    sum added

---
+++ practicing problems/codechef.py
@@ -0,0 +1,15 @@
+Select one of the following options:
+print("PythonProgramming")
+print("Python  ","Programming")
+print("Python","Programming")
+print("Python")
+print("Programming")
+Excellent work!
+
+Correct Answer:
+
+print("Python","Programming")
+Explanation:
+"," is used to separate multiple outputs on the same line inside the print statement
+
+Please note that 2nd and 3rd option are not same. Second option already has trailing space in the first string. So it will print two spaces between the strings.
